1d652b1650929bea900261058a626a55856ef0b4
WeamAlkhateeb <weamalkhateeb360@gmail.com>
2023-06-09 19:02:44 +0300

    wewe update (second container)

--- lib/class/crud.dart
+++ lib/class/crud.dart
@@ -4,7 +4,6 @@ import 'package:weam/class/statusrequest.dart';
 import 'package:http/http.dart' as http;
 import 'package:weam/function/check_internet.dart';
 
-
 class Crud {
   Future<Either<StatusReqest, Map>> postData(String linkurl, Map data) async {
     try {
